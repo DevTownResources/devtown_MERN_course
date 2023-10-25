1cf50f430601ebf410c35e21100c550d64473186
PriyaBihani <priyabihani.official@gmail.com>
2023-10-25 23:20:37 +0530

    Code for ternary operator and for loop

--- src/index.js
+++ src/index.js
@@ -1,44 +1,37 @@
-const age = 21;
+const age = 25;
 
-// if (age >= 18) {
-//   console.log("You are an adult");
-// } else {
-//   console.log("You are a minor");
-// }
+if (age >= 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are a child");
+}
 
-age >= 18 ? console.log("You are an adult") : console.log("You are a minor");
+age >= 18 ? console.log("You are an adult") : console.log("You are a child");
 
-const canVote = age >= 18 ? true : false;
-const canVote1 =
-  age >= 18
-    ? "You can vote"
-    : age < 12
-    ? "You are a child"
-    : "You are a teenager";
-console.log(canVote1);
+const canVote = age >= 18 ? true : false; // ternary operator
 
-// For loops
+console.log(canVote);
+
+// for loops
+
 // for (let i = 0; i < 10; i++) {
-//   // block of code which will be repeated
-
 //   if (i === 7) {
-//     console.log("Number 7 is my favorite number");
+//     console.log("lucky number 7");
 //   } else {
-//     console.log("Number", i);
+//     console.log("Number ", i);
 //   }
 // }
 
 // Nested loops
 
 // for (let i = 0; i < 10; i++) {
-//   console.log("Outer loop", i);
-
 //   for (let j = 0; j < 5; j++) {
-//     console.log(`${i} * ${j} = ${i * j}`);
+//     console.log(`i is ${i} and j is ${j} : ${i * j}`);
 //   }
 // }
 
 // Looping through arrays
+
 const cars = ["Ford", "Chevy", "Honda", "Toyota"];
 
 for (let i = 0; i < cars.length; i++) {
