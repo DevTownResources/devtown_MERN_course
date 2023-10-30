c3d9b645ce32d7c4900bde7cc7921bfb8c4ec96a
PriyaBihani <priyabihani.official@gmail.com>
2023-10-30 21:04:41 +0530

    Nodes and elements

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
-// document.querySelectorAll()
+const list = document.querySelector("ul");
 
-// Returns a NodeList
-const listItems = document.querySelectorAll("li");
+console.log(list.children);
 
-// Access the elements by index
-console.log(listItems);
-// console.log(listItems[1].innerText);
-
-listItems[1].style.color = "red"; // Error
-
-listItems.forEach((item, index) => {
-  item.style.color = "red";
-
-  if (index % 2 === 0) {
-    item.style.backgroundColor = "lightgrey";
-  }
-});
-
-// getElementsByClassName()
-
-const listItems2 = document.getElementsByClassName("item");
-console.log(listItems2);
-console.log(listItems2[0].innerText);
-
-listItems2[0].style.color = "blue";
-
-const listItem2Array = Array.from(listItems2);
-
-listItem2Array.forEach((item) => {
-  item.style.color = "black";
-});
+console.log(list.childNodes);
