efd8eefe721079e195a2e3aab0ee06a4021d9e83
PriyaBihani <priyabihani.official@gmail.com>
2023-11-03 01:05:03 +0530

    Shoppingl list add item to list

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
-const button = document.querySelector("form button");
-const div = document.querySelector("form div:nth-child(2)");
-const form = document.querySelector("form");
+const form = document.querySelector("#item-form");
+const itemInput = document.querySelector("#item-input");
+const itemList = document.querySelector("#item-list");
 
-button.addEventListener("click", (event) => {
-  alert("Button was clicked");
-  //   event.stopPropagation();
-});
+function addItem(e) {
+  e.preventDefault();
 
-div.addEventListener("click", (event) => {
-  alert("Div was clicked");
-  event.stopPropagation();
-});
+  const newItem = itemInput.value;
 
-form.addEventListener("click", (event) => {
-  alert("Form was clicked");
-});
+  //   Validate input
+  if (newItem.trim() === "") {
+    alert("Please enter an item");
+    return;
+  }
 
-document.body.addEventListener("click", (event) => {
-  alert("Body was clicked");
-});
+  // Add new item to list
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+function removeItem(e) {
+  console.dir(e.target);
+}
+
+// Event listeners
+form.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
