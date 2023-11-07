73e2e81f54e6dbf9479a0a5e2fa7903602391045
PriyaBihani <priyabihani.official@gmail.com>
2023-11-07 23:32:29 +0530

    Callback in js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-function changeText() {
-  const heading = document.querySelector("h1");
-  heading.innerHTML = "changed by timer";
+// document.querySelector("button").addEventListener("click", () => {
+//   console.log("this is callback func");
+// });
+
+// function greet(name, callback) {
+//   console.log(`Hello ${name}`);
+//   callback();
+// }
+
+// greet("Priya", () => {
+//   console.log("I am a software developer");
+// });
+// greet("Kartik", () => {
+//   console.log("I am a consultant, and I have good life");
+// });
+
+const posts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
 }
-// const timerId = setTimeout(changeText, 4000);
 
-let counter = 0;
-const intervalId = setInterval(() => {
-  console.log("Hey");
-  if (counter === 5) {
-    clearInterval(intervalId);
-  }
-  counter++;
-}, 3000);
+function createPost(post, cb) {
+  setTimeout(() => {
+    posts.push(post);
+    cb();
+  }, 2000);
+}
 
-document.querySelector("#stop").addEventListener("click", () => {
-  // clearTimeout(timerId);
-  clearInterval(intervalId);
-  console.log("timer stopped");
-});
+createPost({ title: "Post Three", body: "This is post three" }, getPosts);
