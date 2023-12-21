d4c6f2f6d3533d1877c7de8eb597d31d9906d405
PriyaBihani <priyabihani.official@gmail.com>
2023-12-21 22:42:21 +0530

    useEffect in react

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("John");
+
+  const clicked = () => {
+    console.log("clicked");
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", clicked);
+    return () => {
+      document.removeEventListener("click", clicked);
+    };
+  }, [count]);
+
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment +</button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h1> this is name {name}</h1>
     </div>
   );
 }
--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -3,9 +3,15 @@ import { useState, useEffect } from "react";
 function Timer() {
   const [time, setTime] = useState(0);
 
-  setInterval(() => {
-    setTime((prevTime) => prevTime + 1);
-  }, 1000);
+  useEffect(() => {
+    setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+  }, []);
+
+  //   useEffect(() => {
+  //     console.log("time changed");
+  //   });
 
   return <div>{time}</div>;
 }
